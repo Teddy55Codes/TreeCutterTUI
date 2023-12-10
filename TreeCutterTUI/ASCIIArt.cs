b17039e9018131ff19249e28132e3968c6cfad58
Teddy55Codes <teddy@teddy.email>
2023-12-10 21:42:27 +0100

    added ASCII "art"

---
+++ TreeCutterTUI/ASCIIArt.cs
@@ -0,0 +1,16 @@
+﻿namespace TreeCutterTUI;
+
+public static class ASCIIArt
+{
+    public const string Trunck = "|     |";
+    public const string Branch = "-----";
+    public const string NoBranch = "     ";
+    public const string CharacterHead = " o ";
+    public const string CharacterTorso = @"/|\";
+    public const string CharacterLegs = @"/ \";
+    public const string Character = $"{CharacterHead}\n{CharacterTorso}\n{CharacterLegs}";
+
+    public const string TreeSegmentBranchLeft = Branch + Trunck + NoBranch;
+    public const string TreeSegmentBranchRight = NoBranch + Trunck + Branch;
+    public const string TreeSegmentNoBranch = NoBranch + Trunck + NoBranch;
+}
