b5a6fcc0bb1a7fd7008d100d232b0973562d6a45
Teddy55Codes <teddy@teddy.email>
2023-12-10 22:40:35 +0100

    added basic game loop

--- TreeCutterTUI/Program.cs
+++ TreeCutterTUI/Program.cs
@@ -1,7 +1,25 @@
-﻿public class Program
+﻿using TreeCutterTUI;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        
+        var tree = new TreeCutterTUI.Tree(5);
+
+        foreach (var segment in tree)
+        {
+            Console.WriteLine(segment);
+            bool? res = null;
+            while (res == null)
+            {
+                res = Console.ReadKey().Key switch
+                {
+                    ConsoleKey.A or ConsoleKey.LeftArrow => tree.CheckMove(Direction.Left),
+                    ConsoleKey.D or ConsoleKey.RightArrow => tree.CheckMove(Direction.Right),
+                    _ => null
+                };
+            }
+            if (!(bool)res) break;
+        }
     }
 }
