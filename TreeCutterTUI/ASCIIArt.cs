135221417a7b7fcf50e1ba3415adecad0e187f04
Teddy55Codes <teddy@teddy.email>
2023-12-10 22:30:42 +0100

    added ASCII "art" for specific combinations

--- TreeCutterTUI/ASCIIArt.cs
+++ TreeCutterTUI/ASCIIArt.cs
@@ -10,7 +10,37 @@ public static class ASCIIArt
     public const string CharacterLegs = @"/ \";
     public const string Character = $"{CharacterHead}\n{CharacterTorso}\n{CharacterLegs}";
 
-    public const string TreeSegmentBranchLeft = Branch + Trunck + NoBranch;
-    public const string TreeSegmentBranchRight = NoBranch + Trunck + Branch;
-    public const string TreeSegmentNoBranch = NoBranch + Trunck + NoBranch;
+    public const string TreeLayerBranchLeft = Branch + Trunck + NoBranch;
+    public const string TreeLayerBranchRight = NoBranch + Trunck + Branch;
+    public const string TreeLayerNoBranch = NoBranch + Trunck + NoBranch;
+
+    public const string TreeSegmentBranchLeft = $"""
+                                                 {TreeLayerNoBranch}
+                                                 {TreeLayerNoBranch}
+                                                 {TreeLayerNoBranch}
+                                                 {TreeLayerNoBranch}
+                                                 {TreeLayerBranchLeft}
+                                                 """;
+    public const string TreeSegmentBranchRight = $"""
+                                                 {TreeLayerNoBranch}
+                                                 {TreeLayerNoBranch}
+                                                 {TreeLayerNoBranch}
+                                                 {TreeLayerNoBranch}
+                                                 {TreeLayerBranchRight}
+                                                 """;
+    public const string TreeSegmentNoBranch = $"""
+                                               {TreeLayerNoBranch}
+                                               {TreeLayerNoBranch}
+                                               {TreeLayerNoBranch}
+                                               {TreeLayerNoBranch}
+                                               {TreeLayerNoBranch}
+                                               """;
+
+    public const string TreeSegmentCharacter = $"""
+                                                {TreeLayerNoBranch}
+                                                {TreeLayerNoBranch}
+                                                {NoBranch}{Trunck}{CharacterHead}
+                                                {NoBranch}{Trunck}{CharacterTorso}
+                                                {NoBranch}{Trunck}{CharacterLegs}
+                                                """;
 }
